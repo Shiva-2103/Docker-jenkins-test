71c4e56b139d69097ea5f7b95dd17c7bf7a4e24a
Shiva Shanmugam <149222683+Shiva-2103@users.noreply.github.com>
2024-08-12 12:25:02 +0530

    Update and rename NumberCheck.java to SimpleCalculation.java

---
+++ SimpleCalculation.java
@@ -0,0 +1,13 @@
+public class SimpleCalculation {
+    public static void main(String[] args) {
+        // Define two numbers
+        int number1 = 10;
+        int number2 = 20;
+
+        // Calculate the sum of the two numbers
+        int sum = number1 + number2;
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
